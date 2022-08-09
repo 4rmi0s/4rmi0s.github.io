236302a88c026bbb8af93cf9c9a30af019e9531e
h0h0h000 <hohoho00@sch.ac.kr>
2022-08-09 23:40:45 +0900

    2022-08-09_02

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -5,9 +5,21 @@ var store = [{
         "url": "/Security/web1/",
         "teaser": null
       },{
+        "title": "delegate란 무엇인가 ?",
+        "excerpt":"먼저 공부한 내용은 Zeddios 님의 블로그를 참고했음을 밝힙니다.   🍎 delegate   delegate는 객체가 맡은 임무를 위임하는 것을 뜻합니다.   ","categories": ["iOS"],
+        "tags": ["iOS","Swift"],
+        "url": "/iOS/delegate",
+        "teaser": null
+      },{
         "title": "[Life]블로그 기록 start",
         "excerpt":"☝🏻 블로그 시작 전부터 기술 블로그 하나 만들어보자! 라는 생각을 갖고 있었다. 그전에는 노션, velog, 네이버 블로그 등 많은 블로그를 만들고 사용했지만 뭔가 하나씩 마음에 들지 않았다.그렇게 해서 찾게 된 것이 깃 블로그다. ✌🏻 why 깃블로그 ? 깃블로그 장점 1. 자유 내가 알아본 깃 블로그는 다른 블로그보다 자유가 확보된 느낌이였다.물론...","categories": ["Life"],
         "tags": ["Life","start"],
         "url": "/Life/start/",
+        "teaser": null
+      },{
+        "title": "[iOS]Pod 설치 방법",
+        "excerpt":"설치 방법 코코아팟 설치 sudo gem install cocoapods 프로젝트 경로로 이동하기 프로젝트 경로로 이동하는 방법은 많다. 그중에 나는 Finder 를 이용하는 방법을 사용함. Xcode 키고 → 원하는 project에서 오른쪽 마우스 클릭 or 터치패드 두 손가락으로 클릭 → show in Finder → 다시 Finder에서 원하는 project를 선택한 후 오른쪽 마우스 클릭/터치패드...","categories": ["iOS"],
+        "tags": ["iOS","pod"],
+        "url": "/ios/pod",
         "teaser": null
       }]
